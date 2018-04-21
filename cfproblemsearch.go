c8a2045a19184d2e863f693fa0eb16d95151b1c3
levante85 <locci.carlo.85@gmail.com>
2018-04-21 22:27:11 +0200

    added output to pager

--- cfproblemsearch.go
+++ cfproblemsearch.go
@@ -3,13 +3,172 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
 	"strings"
+	"sync"
+	"syscall"
+	"unsafe"
 )
+
+func main() {
+	tflag, err := commandLine()
+	switch {
+	case tflag == nil && err == nil:
+		return
+	case tflag != nil && err == nil:
+		break
+	case tflag == nil && err != nil:
+		fmt.Println(err)
+		return
+	}
+
+	resp, err := readCfProblems(tflag)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	cmd := exec.Command("/usr/bin/less")
+
+	r, stdin := io.Pipe()
+
+	cmd.Stdin = r
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	wg.Add(1)
+	go func(wg *sync.WaitGroup, stdin io.WriteCloser) {
+		defer stdin.Close()
+
+		if resp.Status == "OK" {
+			for i := len(resp.Result.Problems) - 1; i > 0; i-- {
+				p := resp.Result.Problems[i]
+				if p.Type != "PROGRAMMING" {
+					continue
+				}
+
+				fmt.Fprintf(stdin, "|%-4d%-2s|%-50s|%+v\n", p.ContestID, p.Index, p.Name, p.Tags)
+			}
+		} else {
+			fmt.Println("Something went wrong request failed...")
+		}
+
+		wg.Done()
+	}(&wg, stdin)
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg.Wait()
+}
+
+func readCfProblems(tflag *string) (*cfResponse, error) {
+	const api = "http://codeforces.com/api/problemset.problems?tags=%v"
+
+	c := &http.Client{}
+	r, err := c.Get(fmt.Sprintf(api, *tflag))
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	var resp cfResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
+
+func commandLine() (*string, error) {
+	tagFlag := flag.String("tag", "", "problem type ")
+	tagList := flag.Bool("list", false, "list problem tags")
+	flag.Parse()
+
+	switch {
+	case *tagFlag == "" && flag.NFlag() == 0:
+		return nil, errors.New("Tag argument is obligatory to search for problems")
+	case *tagList == true:
+		fmt.Println("List of valid tags: ")
+		for r := bufio.NewReader(strings.NewReader(tags)); ; {
+			l1, _, err := r.ReadLine()
+			if err != nil && err != io.EOF {
+				return nil, err
+			}
+
+			l2, _, err := r.ReadLine()
+			if err != nil && err != io.EOF {
+				return nil, err
+			}
+
+			if io.EOF == err {
+				break
+			}
+
+			fmt.Printf("\t|%-30s|%-30s|\n", l1, l2)
+		}
+
+		return nil, nil
+	}
+
+	return tagFlag, nil
+}
+
+func getTerminalSize() (int, int) {
+	type winsize struct {
+		Row    uint16
+		Col    uint16
+		Xpixel uint16
+		Ypixel uint16
+	}
+
+	ws := &winsize{}
+	retCode, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(syscall.Stdin),
+		uintptr(syscall.TIOCGWINSZ),
+		uintptr(unsafe.Pointer(ws)))
+
+	if int(retCode) == -1 {
+		panic(errno)
+	}
+	return int(ws.Row), int(ws.Col)
+}
+
+type cfResponse struct {
+	Result struct {
+		ProblemStatistics []struct {
+			ContestID   int    `json:"contestId"`
+			Index       string `json:"index"`
+			SolvedCount int    `json:"solvedCount"`
+		} `json:"problemStatistics"`
+		Problems []struct {
+			ContestID int      `json:"contestId"`
+			Index     string   `json:"index"`
+			Name      string   `json:"name"`
+			Points    float64  `json:"points"`
+			Tags      []string `json:"tags"`
+			Type      string   `json:"type"`
+		} `json:"problems"`
+	} `json:"result"`
+	Status string `json:"status"`
+}
 
 var tags = `
 implementation
@@ -47,95 +206,3 @@ fft
 2-sat
 chinese remainder theorem
 schedules`
-
-func main() {
-	tagFlag := flag.String("tag", "", "problem type ")
-	tagList := flag.Bool("list", false, "list problem tags")
-	flag.Parse()
-
-	switch {
-	case *tagFlag == "" && flag.NFlag() == 0:
-		fmt.Println("Tag argument is obligatory to in order to search for problems")
-		return
-	case *tagList == true:
-		fmt.Println("List of valid tags: ")
-		for r := bufio.NewReader(strings.NewReader(tags)); ; {
-			l1, _, err := r.ReadLine()
-			if err != nil && err != io.EOF {
-				fmt.Println(err)
-				return
-			}
-
-			l2, _, err := r.ReadLine()
-			if err != nil && err != io.EOF {
-				fmt.Println(err)
-				return
-			}
-
-			if io.EOF == err {
-				break
-			}
-
-			fmt.Printf("\t|%-30s|%-30s|\n", l1, l2)
-		}
-
-		return
-	}
-
-	const api = "http://codeforces.com/api/problemset.problems?tags=%v"
-
-	c := &http.Client{}
-	r, err := c.Get(fmt.Sprintf(api, *tagFlag))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var resp cfResponse
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if resp.Status == "OK" {
-		for i := len(resp.Result.Problems) - 1; i > 0; i-- {
-			p := resp.Result.Problems[i]
-			if p.Type != "PROGRAMMING" {
-				continue
-			}
-			fmt.Printf("|%-4d%-2s|%-50s|%+v\n", p.ContestID, p.Index, p.Name, p.Tags)
-
-		}
-
-	} else {
-		fmt.Println("Something went wrong request failed...")
-	}
-
-}
-
-type cfResponse struct {
-	Result struct {
-		ProblemStatistics []struct {
-			ContestID   int    `json:"contestId"`
-			Index       string `json:"index"`
-			SolvedCount int    `json:"solvedCount"`
-		} `json:"problemStatistics"`
-		Problems []struct {
-			ContestID int      `json:"contestId"`
-			Index     string   `json:"index"`
-			Name      string   `json:"name"`
-			Points    float64  `json:"points"`
-			Tags      []string `json:"tags"`
-			Type      string   `json:"type"`
-		} `json:"problems"`
-	} `json:"result"`
-	Status string `json:"status"`
-}
